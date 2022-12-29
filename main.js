a9a7bfc6ed87faa349a7f0867fce4be25dcb47d7
Tomas Ruiz <truiz050904@gmail.com>
2022-12-29 01:20:19 -0300

    animations

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 function animate(object) {
-    let skills = document.querySelector(`.${object}`)
-    let pos1 = skills.getBoundingClientRect().top
-    let windowHeight = window.innerHeight / 1.5
+    let skills = document.querySelector(`.${object}`);
+    let pos1 = skills.getBoundingClientRect().top;
+    let windowHeight = window.innerHeight / 1;
 
     if (pos1 < windowHeight) {
-        skills.classList.add("animate")
+        skills.classList.add("animate");
     }
 
 }
 
 addEventListener("scroll", (e) => {
-    animate("skillsContainer")
-    animate("containerProjects")
-})
+    animate("skillsContainer");
+    animate("containerProjects");
+});
 
 
-const inputCheckbox = document.getElementById("mode")
-const main = document.getElementById("main")
+const inputCheckbox = document.getElementById("mode");
+const main = document.getElementById("main");
 inputCheckbox.addEventListener("change", (e) => {
-    main.classList.toggle("light")
-})  
+    main.classList.toggle("light");
+});  
